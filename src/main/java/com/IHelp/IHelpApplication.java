d6b17076180f1352bd23f2ad5d7c542fe75cd2f4
JotaveOliveira <jv.oa202@gmail.com>
2020-01-03 21:13:01 -0300

    Iniciando o projeto

---
+++ src/main/java/com/IHelp/IHelpApplication.java
@@ -0,0 +1,13 @@
+package com.IHelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHelpApplication.class, args);
+	}
+
+}
